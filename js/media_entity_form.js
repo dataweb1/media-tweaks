0decc6ee71b32fb484693cad81c31f86d44c2a7f
dataweb <bert@data-web.be>
2022-10-26 13:53:05 +0200

    Optimize media entity form.

--- js/media_entity_form.js
+++ js/media_entity_form.js
@@ -3,7 +3,11 @@
 
   Drupal.behaviors.media_entity_form = {
     attach: function (context) {
-
+      console.log('ok?');
+      // Move 'Rename file' to joing the 'delete' button in Claro.
+      $('[name$="_remove_button"]').appendTo('.form-managed-file__main');
+      $('.form-managed-file .file').appendTo('.image-preview__img-wrapper');
+      $('.form-managed-file .file a').attr('target', '_blank');
     }
   };
 
