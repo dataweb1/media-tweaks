87779136ae837fb030d98b262bf4a62188e86742
dataweb <bert@data-web.be>
2022-11-22 12:52:12 +0100

    File rename button tweaks.

--- modules/file_rename_tweaks/js/file_rename_tweaks.js
+++ modules/file_rename_tweaks/js/file_rename_tweaks.js
@@ -4,7 +4,19 @@
   Drupal.behaviors.rename_file_tweaks = {
     attach: function (context) {
       // Move 'Rename file' to joing the 'delete' button in Claro.
-      $('.button-file-rename').appendTo('.form-managed-file__main');
+
+      $('.button-file-rename').once().each(function() {
+        if ($('.media-library-add-form-wrapper').length > 0) {
+          let w = $(this).closest('details');
+          console.log(w.length);
+
+          $(this).appendTo(w.find('.form-item-name'));
+        }
+        else {
+          let w = $(this).parent().find('.form-managed-file__meta-wrapper');
+          $(this).appendTo(w.find('.file--image'));
+        }
+      });
 
     }
   };
