11270e9b26cb53dd72dce2daf58e82ec2354a2af
dataweb <bert@data-web.be>
2022-11-22 17:39:46 +0100

    Last CSS tweaks to match media edit and media add form, part 3.

--- js/media_entity_form.js
+++ js/media_entity_form.js
@@ -5,7 +5,7 @@
     attach: function (context) {
       console.log('ok?');
       // Move 'Rename file' to joing the 'delete' button in Claro.
-      $('[name$="_remove_button"]').appendTo('.form-managed-file__main');
+      $('.form-managed-file [name$="_remove_button"]').appendTo('.form-managed-file__main');
       $('.form-managed-file .file').appendTo('.image-preview__img-wrapper');
       $('.form-managed-file .file a').attr('target', '_blank');
     }
